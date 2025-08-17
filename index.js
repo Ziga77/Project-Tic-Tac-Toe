952cf11d0ee856f4c1699165be01562007163f55
Ziga <ziga.kljun2@gmail.com>
2025-08-17 12:14:48 +0200

    Add get board and switch player function

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
-console.log('hi');
-
-/*function gameBoard() {
-  const rows = 3;
-  const columns = 3;
-  const board = [];
-
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < columns; j++) {
-      board[i].push('');
-    }
-  }
-
-  return board;
-}*/
-
 const gameBoard = () => {
   const rows = 3;
   const columns = 3;
   const board = [];
 
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < columns; j++) {
-      board[i].push('');
+  function getBoard() {
+    for (let i = 0; i < rows; i++) {
+      board[i] = [];
+      for (let j = 0; j < columns; j++) {
+        board[i].push('');
+      }
     }
+    return board;
   }
 
   function firstPlayer() {
-    let player;
+    let currentPlayer;
 
     if (Math.random() >= 0.51) {
-      player = 1;
+      currentPlayer = 1;
     } else {
-      player = 0;
+      currentPlayer = 0;
     }
-    return player;
+    return currentPlayer;
   }
 
-  function makeMove(player, row, col) {
+  function makeMove(currentPlayer, row, col) {
     if (row < 0 || row >= rows || col < 0 || col >= columns) {
       return false;
     }
@@ -46,9 +32,7 @@ const gameBoard = () => {
       return false;
     }
 
-    let symbol;
-
-    if (player === 0) {
+    if (currentPlayer === 0) {
       symbol = 'x';
     } else {
       symbol = 'o';
@@ -59,18 +43,38 @@ const gameBoard = () => {
     return symbol;
   }
 
+  function switchPlayer(currentPlayer) {
+    if (currentPlayer === 0) {
+      currentPlayer = 1;
+    } else {
+      currentPlayer = 0;
+    }
+    return currentPlayer;
+  }
+
   return {
-    board,
+    getBoard,
     firstPlayer,
     makeMove,
+    switchPlayer,
   };
 };
 
 const game = gameBoard();
 
-console.log(game.board);
+game.getBoard();
+console.log(game.getBoard());
 
-console.log(game.makeMove(game.firstPlayer(), 0, 2));
-console.log(game.makeMove(game.firstPlayer(), 1, 2));
-console.log(game.makeMove(game.firstPlayer(), 2, 2));
-console.log(game.makeMove(game.firstPlayer(), 0, 1));
+let currentPlayer = game.firstPlayer();
+
+console.log(game.makeMove(currentPlayer, 0, 2));
+currentPlayer = game.switchPlayer(currentPlayer);
+
+console.log(game.makeMove(currentPlayer, 1, 2));
+currentPlayer = game.switchPlayer(currentPlayer);
+
+console.log(game.makeMove(currentPlayer, 2, 2));
+currentPlayer = game.switchPlayer(currentPlayer);
+
+console.log(game.makeMove(currentPlayer, 0, 1));
+currentPlayer = game.switchPlayer(currentPlayer);
