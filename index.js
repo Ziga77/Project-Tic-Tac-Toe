f7237da183e0b7ae63ddf6ba690ba9f29a13fca9
Ziga <ziga.kljun2@gmail.com>
2025-08-19 20:19:47 +0200

    Add check winner module.

--- index.js
+++ index.js
@@ -75,7 +75,38 @@ function gameController(player1, player2, board) {
   };
 }
 
-function checkWinner() {}
+function checkWinner(board, symbol) {
+  for (let i = 0; i < 3; i++) {
+    if (
+      board[0][i] === symbol &&
+      board[1][i] === symbol &&
+      board[2][i] === symbol
+    ) {
+      return true;
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (
+      board[i][0] === symbol &&
+      board[i][1] === symbol &&
+      board[i][2] === symbol
+    ) {
+      return true;
+    }
+  }
+
+  if (
+    (board[0][0] === symbol &&
+      board[1][1] === symbol &&
+      board[2][2] === symbol) ||
+    (board[0][2] === symbol && board[1][1] === symbol && board[2][0] === symbol)
+  ) {
+    return true;
+  }
+
+  return false;
+}
 
 function displayController() {}
 
